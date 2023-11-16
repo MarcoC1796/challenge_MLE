06beaf4e650b9aa688d3837d8536b424226aae5c
Marco Chacon <marco.chacon1796@gmail.com>
2023-11-15 21:56:44 -0600

    implementing predict enpoint

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,22 @@
-# syntax=docker/dockerfile:1.2
-FROM python:latest
-# put you docker configuration here
+# Use the official Python 3.9 image as the base image
+FROM python:3.9-slim
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy the requirements files into the container
+COPY requirements*.txt ./
+
+# Install the required dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install --no-cache-dir -r requirements-dev.txt
+RUN pip install --no-cache-dir -r requirements-test.txt
+
+# Copy the entire project directory into the container
+COPY . .
+
+# Expose the port your FastAPI app will run on (adjust as needed)
+EXPOSE 80
+
+# Command to run your FastAPI app
+CMD ["uvicorn", "challenge.api:app", "--host", "0.0.0.0", "--port", "80"]
--- challenge/api.py
+++ challenge/api.py
@@ -1,13 +1,53 @@
 import fastapi
+from pydantic import BaseModel
+from typing import List
+import logging
+import pandas as pd
 
-app = fastapi.FastAPI()
+from .model import DelayModel
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+app = fastapi.FastAPI(debug=True)
+
+
+class Flight(BaseModel):
+    OPERA: str
+    TIPOVUELO: str
+    MES: int
+
+
+class FlightList(BaseModel):
+    flights: List[Flight]
+
+
+delay_model = DelayModel()
+
 
 @app.get("/health", status_code=200)
 async def get_health() -> dict:
-    return {
-        "status": "OK"
-    }
+    return {"status": "OK"}
+
 
 @app.post("/predict", status_code=200)
-async def post_predict() -> dict:
-    return
+async def post_predict(flight_list: FlightList) -> dict:
+     try:
+        data = pd.DataFrame([flight.dict() for flight in flight_list.flights])
+
+        logging.debug(f"Incoming prediction request with data: {data}")
+
+        features = delay_model.preprocess(data)
+        predictions = delay_model.predict(features)
+
+        logging.info(f"Predictions: {predictions}")
+
+        return {"predict": predictions}
+    except fastapi.HTTPException as e:
+        logging.error(f"HTTP Exception: {e.detail}")
+        raise
+    except Exception as e:
+        logging.error(f"Error during prediction: {str(e)}", exc_info=True)
+        raise fastapi.HTTPException(
+            status_code=500, detail="An error occurred during prediction."
+        )
